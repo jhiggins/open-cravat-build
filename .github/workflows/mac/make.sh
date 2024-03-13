bf2b6514584485ea90f77d07ed3f94db604f45f1
jhiggins@potomacitgroup.com <jhiggins@potomacitgroup.com>
2024-03-13 14:01:24 -0400

    Update to run full process

---
+++ .github/workflows/mac/make.sh
@@ -0,0 +1,32 @@
+# conda should be installed.
+# python3.10 environment should be setup.
+# Change the variables below as needed.
+CONDADIR=`realpath ../miniconda`
+ENVNAME=py3
+APPDIR=./OpenCRAVAT.app
+LAUNCHDIR=./launchers
+
+if [ -z $1 ]; then
+	echo "What is the version of open-cravat?"
+	exit
+fi
+
+source $CONDADIR/etc/profile.d/conda.sh
+conda activate $ENVNAME
+
+pip uninstall open-cravat -y
+pip install open-cravat==$1
+
+ENVDIR=$CONDADIR/envs/$ENVNAME
+RESDIR=$APPDIR/Contents/Resources
+rm -rf $RESDIR/bin $RESDIR/conda-meta $RESDIR/include $RESDIR/lib $RESDIR/bin $RESDIR/man $RESDIR/share $RESDIR/ssl
+cp -R $ENVDIR/* $RESDIR/
+cp -R $LAUNCHDIR $RESDIR/
+
+cp $APPDIR/Contents/Info.plist $RESDIR/Info.plist.bak
+
+plutil -replace CFBundleShortVersionString -string $1 $APPDIR/Contents/Info.plist
+
+
+cd $RESDIR/bin
+sed -i '' "s=${ENVDIR}/bin/python=/usr/bin/env python -I=g" oc
